d3f06435f3c28ee9c8d469714609e9e927b91bcd
ynksvn <ysevin@gmail.com>
2022-07-01 20:42:32 -0500

    Third project is created at master, updated

--- src/TC003.java
+++ src/TC003.java
@@ -1,5 +1,5 @@
 public class TC003 {
     public static void main(String[] args) {
-        System.out.println("Third project, wishing for the conflict");
+        System.out.println("Third project");
     }
 }
